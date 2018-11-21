0ba702ebae0be4f1a9a78fa8ae88a869de23cb16
Vessiik <44364703+Vessiik@users.noreply.github.com>
2018-11-21 11:53:07 +0200

    Add files via upload

---
+++ chap 8/task 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_4 //problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter decimal number: ");
+            int deci = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("{0} to binary is {1}.",
+                deci, deci.ToString("deci, 2"));
+        }
+    }
+}
---
+++ chap 8/task 7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter hexadecimal number: ");
+            string hexa = Console.ReadLine();
+            Console.WriteLine("{0} to decimal is {1}.",
+                hexa, Convert.ToInt32(hexa, 16));
+        }
+    }
+}
